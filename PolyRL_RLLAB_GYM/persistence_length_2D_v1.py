a1582d14f78e570f184253ef58d1bae49a7cd4a7
Riashat <riashat.islam.93@gmail.com>
2017-04-13 14:50:18 -0400

    PolyRL Exp Runs

--- PolyRL_RLLAB_GYM/persistence_length_2D_v1.py
+++ PolyRL_RLLAB_GYM/persistence_length_2D_v1.py
@@ -17,6 +17,10 @@ import pickle as pickle
 import numpy as np
 import pyprind
 import lasagne
+import rllab.misc.logger as logger
+
+import random 
+import pandas as pd 
 
 
 def parse_update_method(update_method, **kwargs):
@@ -102,14 +106,14 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
         env, 
         qf, 
         policy,
-        L_p=50, 
-        b_step_size=0.5, 
-        sigma=0.05, 
-        max_exploratory_steps = 10, 
-        epoch_length=10000,
+        L_p=1, 
+        b_step_size=0.005, 
+        sigma = 0.1, 
+        max_exploratory_steps = 100, 
+        epoch_length=1000,
         length_polymer_chain=100000,
         batch_size=32,
-        max_path_length=10000,
+        max_path_length=200000,
         qf_weight_decay=0.,
         qf_update_method='adam',
         qf_learning_rate=1e-3,
@@ -201,9 +205,7 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
         path_length=0
 
         chain_actions = np.array([self.initial_action])
-        chain_states = self.initial_state
-
-        theta = np.random.uniform(-np.pi, np.pi, 1)
+        chain_states = np.array([self.initial_state])
 
         action_trajectory_chain= 0
         state_trajectory_chain = 0
@@ -213,36 +215,57 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
         itr = 0
         terminal = False
 
-        """
-        RANDOM H_X AND H SHOULD NOT BE SAME ALL EPISODE
-        """
         h_x = 0.001
-
         h_y = np.sqrt(np.absolute(self.b_step_size**2 - h_x**2))
-
         H = np.array([h_x,  h_y])
 
-        next_action = self.initial_action + H
+        all_H = np.array([H])
+        all_theta = np.array([])
 
         sample_policy = pickle.loads(pickle.dumps(self.policy))
+
+        self.initial_action = self.env.action_space.sample()
+
+        # print ("Starting Initial Action", self.initial_action)
+
+        last_action_chosen = self.initial_action
+
 
         for itr in range(self.max_exploratory_steps):
 
             print ("LP Exploration Episode", itr)
-
             print ("Replay Buffer Sample Size", pool.size)
 
-            #should start from a new random action at every LP Exploration phase?
+            """
+            Using same H in all iterations
+            """
 
-            self.initial_action = self.env.action_space.sample()
+            h_flip_x = np.random.randint(-5,4,1)
+            h_flip_y = np.random.randint(-5,4,1)
 
-            print ("Initial Action of LP Exploration", self.initial_action)
+            if h_flip_x > 0:
+                h_flip_x = 1
+            else:
+                h_flip_x = -1
 
-            h_x = 0.001
-            h_y = np.sqrt(np.absolute(self.b_step_size**2 - h_x**2))
+            if h_flip_y > 0:
+                h_flip_y = 1
+            else:
+                h_flip_y = -1
+
+            h_x = np.random.uniform(0, self.b_step_size) 
+            h_y = np.sqrt(self.b_step_size**2 - h_x**2)
+
+            h_x = h_flip_x * h_x
+            h_y = h_flip_y * h_y
+
             H = np.array([h_x,  h_y])
 
-            next_action = self.initial_action + H
+
+
+            next_action = last_action_chosen + H
+
+
 
             for epoch_itr in pyprind.prog_bar(range(self.epoch_length)):
 
@@ -257,11 +280,13 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
 
 
                 theta_mean = np.arccos( np.exp(   np.true_divide(-self.b_step_size, self.L_p) )  )
+
+
                 theta = np.random.normal(theta_mean, self.sigma, 1)   
+                all_theta = np.append(all_theta, theta)
+
 
                 coin_flip = np.random.randint(2, size=1)
-
-
                 if coin_flip == 0:
                     operator = np.array([[np.cos(theta), - np.sin(theta)], [np.sin(theta),  np.cos(theta)]]).reshape(2,2)
                 elif coin_flip == 1:
@@ -272,6 +297,10 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
                 phi_t_1 = phi_t + np.dot(operator, H)
 
                 chosen_action = np.array([phi_t_1])
+
+                # print ("Current Action", phi_t)
+                # print ("Next Action", chosen_action)
+
                 chain_actions = np.append(chain_actions, chosen_action, axis=0)
 
 
@@ -280,14 +309,23 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
                 - also make sure same scaling is done in DDPG without polyRL algo
                 """
                 chosen_state, reward, terminal, _ = self.env.step(chosen_action)
+                # print("Reward for action", reward)
 
+
+                ### if an invalid state is reached
                 if terminal == True:
-                    print ("Reward at Invalid State", reward)
-                    print ("Number of Steps before invalid state", epoch_itr)
+                    #start a new trajectory during the exploration phase
+                    last_action_chosen = self.env.action_space.sample()
+
+                    """
+                    Add the tuple of invalid state, a and r(with large penalty negative reward to replay buffer)
+                    put negative reward of -1
+                    """
+                    print ("************Reward at Invalid State", reward)
+                    print ("************Number of Steps before invalid state", epoch_itr)
                     break
 
-
-                chain_states = np.append(chain_states, chosen_state)    
+                chain_states = np.append(chain_states, np.array([chosen_state]), axis=0)    
 
                 action = chosen_action
                 state = chosen_state
@@ -295,39 +333,33 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
                 end_traj_state = chosen_state
                 end_traj_action = chosen_action
 
-
                 #updates to be used in next iteration
                 H = phi_t_1 - phi_t
+                all_H = np.append(all_H, np.array([H]), axis=0)
                 next_action = phi_t_1
 
                 path_length += 1
 
-
-
                 if not terminal and path_length >= self.max_path_length:
 
                     terminal = True
+                    terminal_state = chosen_state
 
-                    print ("LP Exploration terminated")
+                    print ("LP Epoch Length Terminated")
 
                     path_length = 0
                     path_return = 0
                     state = self.env.reset()
                     sample_policy.reset()
-
-                    print ("Reward at terminal state", reward)
                     print ("Step Number at Terminal", epoch_itr)
 
                     # only include the terminal transition in this case if the flag was set
                     if self.include_horizon_terminal_transitions:
-
                         #### adding large negative reward to the terminal state reward??? Check this
                         pool.add_sample(state, action, reward * self.scale_reward*100, terminal)
-                
                     break
 
                 else:
-
                     pool.add_sample(state, action, reward * self.scale_reward, terminal)
         
 
@@ -340,11 +372,29 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
 
                 itr += 1
 
+            last_action_chosen = action
+            last_action_chosen = last_action_chosen[0, :]
+
+
 
         action_trajectory_chain = chain_actions
         state_trajectory_chain = chain_states
         end_trajectory_action = end_traj_action
         end_trajectory_state = end_traj_state
+
+
+
+        # df_a = pd.DataFrame(action_trajectory_chain, columns=['Dim 1', 'Dim 2'])
+        # df_a.to_csv("exploratory_action.csv")
+
+        # df_s = pd.DataFrame(state_trajectory_chain, columns=['Dim 1', 'Dim 2', 'Dim 3', 'Dim 4', 'Dim 5', 'Dim 6', 'Dim 7', 'Dim 8', 'Dim 9', 'Dim 10', 'Dim 11', 'Dim 12', 'Dim 13'])
+        # df_s.to_csv("exploratory_states.csv")
+
+        # df = pd.DataFrame({'Theta Values': all_theta,
+        #            'H(dim 1)': all_H[1:, 0],
+        #            'H(dim 2)': all_H[1:, 1]})
+        # df.to_csv("All Exploration Params.csv")
+
 
         return updated_q_network, action_trajectory_chain, state_trajectory_chain, end_trajectory_action, end_trajectory_state
 
@@ -449,14 +499,19 @@ class Persistence_Length_Exploration(ExplorationStrategy, Serializable):
             target_qf.get_param_values() * (1.0 - self.soft_target_tau) +
             self.qf.get_param_values() * self.soft_target_tau)
 
+
+        """
+        Check this again - whether Q network is actually updated or not
+        """
+
+
+
         self.qf_loss_averages.append(qf_loss)
         self.policy_surr_averages.append(policy_surr)
         self.q_averages.append(qval)
         self.y_averages.append(ys)
 
         q_network_exploratory_update = self.qf
-
-
 
         return q_network_exploratory_update
 
